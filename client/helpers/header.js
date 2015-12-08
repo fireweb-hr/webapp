b8b8143da1dd11887349d3714dd67e647aa4f089
Jori <joriregter@gmail.com>
2015-12-08 09:38:11 +0100

    Change file positions

--- client/helpers/header.js
+++ client/helpers/header.js
@@ -1,17 +1,3 @@
-Template.header.events({
-    "click #js-mobile-menu": function () {
-        //
-        $("#js-navigation-menu").slideToggle(function(){
-            if ($('#js-navigation-menu').is(':hidden')) {
-                $('#js-navigation-menu').removeAttr('style');
-            }
-        });
-        e.preventDefault();
-        return false;
-    }
-});
-
 Template.header.onRendered(function () {
-    // Don't show when first time from transitioning into media query.
-    $('#js-navigation-menu').removeClass("show");
+    $(".button-collapse").sideNav();
 });
--- client/views/header/header.js
+++
@@ -1,3 +0,0 @@
-Template.header.onRendered(function () {
-    $(".button-collapse").sideNav();
-});
