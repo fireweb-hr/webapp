d3920eff6ca5b71c4e3900fe2c8c9c7bbad11dec
Jori <joriregter@gmail.com>
2015-12-08 20:56:12 +0100

    Adds reactive switching between markers in maps

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,5 +1,9 @@
 Template.map.helpers({
-    dashBoardMapOptions: function () {
+    node: function () {
+        // Eventually get info from database.
+        return Session.get("selectedNode");
+    },
+    dashboardMapOptions: function () {
         if (GoogleMaps.loaded()) {
             return {
                 center: new google.maps.LatLng(52.7000,4.7000),
@@ -9,10 +13,42 @@ Template.map.helpers({
     }
 });
 
-Template.map.events(function () {
-
-});
-
 Template.map.onRendered(function () {
     GoogleMaps.load();
+});
+
+Template.map.onCreated(function () {
+    GoogleMaps.ready('dashboardMap', function(map) {
+        var that = this;
+
+        // Eventually get nodes from database with id and lat lng.
+        var markerOptions = [
+            new google.maps.Marker({
+                position: map.options.center,
+                map: GoogleMaps.maps.dashboardMap.instance,
+                title: "I am a demo marker.",
+                id: 1
+            }),
+            new google.maps.Marker({
+                position: {lat: 52.7400, lng: 4.700},
+                map: GoogleMaps.maps.dashboardMap.instance,
+                title: "I am a demo marker.",
+                id: 2
+            })
+
+        ];
+
+        this.addMarker = function (options) {
+            var marker = options;
+            marker.addListener("click", function () {
+                Session.set("selectedNode", this.id);
+            });
+        };
+
+        _.each(markerOptions, function (marker) {
+            that.addMarker(marker);
+        });
+
+        Session.set("selectedNode", 1);
+    });
 });
