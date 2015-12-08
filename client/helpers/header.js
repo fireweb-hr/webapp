bd1720b49872a84e7f452d231d319bc1f7fa3d70
Jori <joriregter@gmail.com>
2015-12-08 15:05:07 +0100

    Adds logout

--- client/helpers/header.js
+++ client/helpers/header.js
@@ -1,3 +1,10 @@
+Template.header.events({
+    "click .logout": function (e) {
+        e.preventDefault();
+        Meteor.logout();
+        Router.go("/login");
+    }
+})
 Template.header.onRendered(function () {
     $(".button-collapse").sideNav();
 });
--- client/helpers/login.js
+++ client/helpers/login.js
@@ -2,13 +2,18 @@ Template.login.events({
     "submit form": function (e) {
         e.preventDefault();
 
-        var username = event.target.loginEmail.value;
-        var passwordVar = event.target.loginPassword.value;
+        var c = {
+            email: event.target.loginEmail.value,
+            password: event.target.loginPassword.value
+        }
 
-        Meteor.loginWithPassword(username, passwordVar, function (err) {
-            if (!err)
-                Router.go("/");
+        Meteor.loginWithPassword(c.email, c.password, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            Router.go("/dashboard");
         });
-
     }
 })
